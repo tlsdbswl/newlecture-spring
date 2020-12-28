c5a7a24a40137e00f21af5eb675caf55932610e0
tlsdbswl <tlsdbswlek@gmail.com>
2020-12-28 15:28:34 +0900

    스프링 MVC 23강까지 소스코드 추가

---
+++ webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.newlecture.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+//     http://.../WEB
+public class IndexController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		ModelAndView mv = new ModelAndView("root.index");
+		mv.addObject("data", "Hello Spring MVC~");
+		//mv.setViewName("/WEB-INF/view/index.jsp");
+		return mv;
+	}
+	
+}
